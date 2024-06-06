9af94d1d0f1f33d23114fe01838e23ed683140ba
Idowu Disu <idowud.disu@yahoo.com>
2024-06-06 14:27:20 -0500

    component updates

--- force-app/main/default/lwc/nav/nav.js
+++ force-app/main/default/lwc/nav/nav.js
@@ -1,6 +1,34 @@
 import { LightningElement } from 'lwc';
 import IMAGES from '@salesforce/resourceUrl/Images';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class Nav extends LightningElement {
+export default class Nav extends NavigationMixin(LightningElement) {
     logoUrl = IMAGES + '/EERE.png';
+
+    navigateToHome() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__namedPage',
+            attributes: {
+                pageName: 'Home'
+            }
+        });
+    }
+
+    navigateToContact() {
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Contact_Us__C'
+            }
+        });
+    }
+
+    navigatetoLogin(){
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Login'
+            }
+        });
+    }
 }
--- force-app/main/default/lwc/sideNav1/sideNav1.js
+++ force-app/main/default/lwc/sideNav1/sideNav1.js
@@ -2,27 +2,37 @@ import { LightningElement } from 'lwc';
 import ApplicationModal from 'c/applicationModal';
 import ConceptPaperModal from 'c/conceptPaperModal';
 import ProgressReportForm from 'c/progressReportForm';
+import { NavigationMixin} from 'lightning/navigation';
 
-export default class SideNav1 extends LightningElement {
+export default class SideNav1 extends NavigationMixin(LightningElement) {
    
     async handleClick(){
         const result = await ConceptPaperModal.open({
-            size: "Medium",
+            size: "small",
             description: "Modal for application component"
         });
     }
 
     async handleClick2(){
         const result = await ApplicationModal.open({
-            size: "Medium",
+            size: "small",
             description: "Modal for application component"
         });
     }
 
     async handleClick3(){
         const result = await ProgressReportForm.open({
-            size: "Medium",
+            size: "small",
             description: "Modal for application component"
+        });
+    }
+
+    navigateToHome() {
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Home'
+            }
         });
     }
 }
